be0b59a1fbee6b2cf0540758c90365e24383a97d
Zack Buhman <zack@buhman.org>
2017-10-25 16:46:26 -0500

    short-ui: initial create-elm-app

---
+++ short-ui/src/Main.elm
@@ -0,0 +1,54 @@
+module Main exposing (..)
+
+import Html exposing (Html, text, div, img)
+import Html.Attributes exposing (src)
+
+
+---- MODEL ----
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ div [] [ text "It works!" ]
+        ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { view = view
+        , init = init
+        , update = update
+        , subscriptions = always Sub.none
+        }
